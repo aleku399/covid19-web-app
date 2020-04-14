ba4b242b6ba27c8a4389848eb9da1b15df7abc32
aleku399 <aleku399@gmail.com>
2020-04-14 09:59:53 +0300

    Updates

---
+++ precache-manifest.2b9090eff1c0968b9e2a942d0c385781.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b83f16a5529f7c6dae8aedc025728f2",
+    "url": "/covi19-web-app/index.html"
+  },
+  {
+    "revision": "db54c6ec57aa6f99e8a5",
+    "url": "/covi19-web-app/static/css/main.64d447a4.chunk.css"
+  },
+  {
+    "revision": "0746e215336676c9bcf7",
+    "url": "/covi19-web-app/static/js/2.88a394fb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covi19-web-app/static/js/2.88a394fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db54c6ec57aa6f99e8a5",
+    "url": "/covi19-web-app/static/js/main.c98efda4.chunk.js"
+  },
+  {
+    "revision": "bde61b108b7e93de01d9",
+    "url": "/covi19-web-app/static/js/runtime-main.a2da66d5.js"
+  }
+]);
